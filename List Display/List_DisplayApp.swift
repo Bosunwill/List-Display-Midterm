7b2b3210d39c641cf5299251efae072c68ec4ac8
Bosunwill <54271663+Bosunwill@users.noreply.github.com>
2021-03-25 19:20:46 -0500

    Initial Commit

---
+++ List Display/List_DisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_DisplayApp.swift
+//  List Display
+//
+//  Created by Gui Ndikum on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct List_DisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
